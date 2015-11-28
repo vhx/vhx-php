4552e66a3ae3c264ca608c7e710e825dc8209bde
Scott Robertson <scott@scottusrobus.com>
2015-11-28 12:23:47 -0800

    remove unsued defaults

--- lib/api.php
+++ lib/api.php
@@ -7,8 +7,6 @@ class API
   public static $key;
   const HOST = 'api.crystal.dev';
   const PROTOCOL = 'http://';
-  const PORT = '7000';
-  const BASE_PATH = '/v1/';
   const API_VERSION = null;
 
   public static function setKey($api_key) {
