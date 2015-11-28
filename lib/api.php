00dd3e59905ea5f42037d24c19bad3c048fbc779
Scott Robertson <scott@scottusrobus.com>
2015-11-28 12:26:07 -0800

    use production urls!

--- lib/api.php
+++ lib/api.php
@@ -5,8 +5,8 @@ namespace VHX;
 class API
 {
   public static $key;
-  const HOST = 'api.crystal.dev';
-  const PROTOCOL = 'http://';
+  const HOST = 'api.vhx.tv';
+  const PROTOCOL = 'https://';
   const API_VERSION = null;
 
   public static function setKey($api_key) {
