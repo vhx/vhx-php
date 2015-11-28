231b8d3b5cc1e7d7e4ac1c9e98164db2d1760e83
Scott Robertson <scott@scottusrobus.com>
2015-11-28 12:24:14 -0800

    error classes

--- lib/errors/resourceNotFound.php
+++ lib/errors/resourceNotFound.php
@@ -2,7 +2,7 @@
 
 namespace VHX\Error;
 
-class ApiConnection extends Base {
+class ResourceNotFound extends Base {
   public function __construct($message = null, $http_status = null, $http_body = null, $json_body = null, $http_headers = null) {
     parent::__construct($message, $http_status, $http_body, $json_body, $http_headers);
   }
