6e5872c374e9856ebdf30a2ba3da9b93211ce710
Scott Robertson <scott@scottusrobus.com>
2015-11-28 16:40:46 -0800

    update version

--- lib/api.php
+++ lib/api.php
@@ -7,7 +7,7 @@ class API
   public static $key;
   const HOST = 'api.vhx.tv';
   const PROTOCOL = 'https://';
-  const API_VERSION = null;
+  const VERSION = '0.1.0-beta.3';
 
   public static function setKey($api_key) {
     self::$key = $api_key;
