1a09b5ca641f9bceca97e09afbf1223248e52575
Scott Robertson <scott@scottusrobus.com>
2015-12-21 20:20:53 -0800

    add video files method, bump version

--- lib/Videos.php
+++ lib/Videos.php
@@ -6,6 +6,9 @@ class Videos extends Resource {
   public static function all($params = array()) {
     return self::_list($params);
   }
+  public static function files($id = null, $params = array()) {
+    return self::_files($id, $params);
+  }
   public static function retrieve($id = null) {
     return self::_retrieve($id);
   }
