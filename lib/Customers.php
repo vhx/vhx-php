62a9cc1f542abf9a970254c2803fe8c015592818
Scott Robertson <scott@scottusrobus.com>
2016-02-15 22:54:37 -0800

    add update method for customers resource

--- lib/Customers.php
+++ lib/Customers.php
@@ -6,6 +6,9 @@ class Customers extends Resource {
   public static function all($params = array()) {
     return self::_list($params);
   }
+  public static function update($id = null) {
+    return self::_update($id);
+  }
   public static function retrieve($id = null) {
     return self::_retrieve($id);
   }
